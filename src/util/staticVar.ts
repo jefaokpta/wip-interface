80aefe13d69e5c1f6127a3299c427cb6c25de9f6
jefaokpta <jefaokpta@hotmail.com>
2023-07-20 19:43:12 -0300

    vamos testar

--- src/util/staticVar.ts
+++ src/util/staticVar.ts
@@ -1,5 +1,4 @@
 export const urlBase = process.env.URL_BASE ?? 'http://localhost:8080'
 export const mediaFolder = 'whatsappFiles/medias'
-export const authFolder = 'whatsappFiles/auths'
 
 /** ATENCAO NAO ESQUECA DO COMANDO TSC!!! */
