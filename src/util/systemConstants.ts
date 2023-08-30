06b12e23d0d5ee9fa17e3662b4d128fb174691d8
jefaokpta <jefaokpta@hotmail.com>
2023-08-30 16:19:30 -0300

    atualizando s3 de uploads

--- src/util/systemConstants.ts
+++ src/util/systemConstants.ts
@@ -1,6 +1,6 @@
 export const WIP_API_URL = process.env.WIP_API_URL ?? 'http://localhost:8080'
 export const CONTROL_NUMBER = Number(process.env.CONTROL_NUMBER ?? 100023)
-export const UPLOAD_FOLDER_URL = 'https://d22uovf8poqnbu.cloudfront.net/uploads'
+export const UPLOAD_FOLDER_URL = 'https://d3mqe4ehhhyqta.cloudfront.net/uploads'
 export const BUCKET_S3 = 'wip-medias'
 
 /** ATENCAO NAO ESQUECA DO COMANDO TSC!!! */
